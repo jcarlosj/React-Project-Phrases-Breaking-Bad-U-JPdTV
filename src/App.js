27ef37d4a85fea6a550fafe1c28dc0d0a4479920
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-06 18:16:02 -0500

    [+] NPM > @emotion/<core,styled>: Implementa Style Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-//import logo from './logo.svg';
 import logo from './breaking-bad-logo.svg';
 import './App.css';
+import styled from '@emotion/styled';
+
+const Button = styled .button `
+  background: #085D2E;
+  border: 1px solid black;
+  font-size: 2rem;
+  color: white;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+`;
 
 function App() {
+
+  const getApiData = () => {
+    console .log( 'API Data' );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="Breaking Bad" />
+        <Button 
+          onClick={ () => getApiData() }
+        >New phrase</Button>
       </header>
     </div>
   );
